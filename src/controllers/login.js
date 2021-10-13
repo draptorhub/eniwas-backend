56f299ee0e5fa4a11a555733746c44fe99f5f2e6
AnirudhKumaran <draptorteam@gmail.com>
2021-10-13 17:23:10 +0530

    login query changed

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -7,10 +7,11 @@ controllers.managerAuthorize = async (req, res) => {
 
   let mngrId = req.body.mid
   let mngrPass = req.body.mpass
+  let branchId = req.body.bid
 
   var sql = `select b.branchId,m.mngrId from branches b,manager m 
              where m.mngrId=b.managerId and m.mngrId='${mngrId}' 
-             and m.mngrPass='${mngrPass}';`
+             and m.mngrPass='${mngrPass}' and b.branchId='${branchId}';`
 
   let data = await sequelize.query(sql,{
               type: sequelize.QueryTypes.SELECT
