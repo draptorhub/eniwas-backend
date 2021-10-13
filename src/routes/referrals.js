809d305978c72276331990e576fe02a9bb055b24
AnirudhKumaran <draptorteam@gmail.com>
2021-10-13 13:15:19 +0530

    referral APIs
    all the referral apis and then checking if the pm2 automatically takes the github changes

--- src/routes/referrals.js
+++ src/routes/referrals.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const router=express.Router()
+<<<<<<< Updated upstream
 const referrals=require('../controllers/referrals')
 
 router.post('/create',referrals.create)
@@ -7,5 +8,14 @@ router.get('/get/:reffid',referrals.get)
 router.get('/list',referrals.list)
 router.put('/update',referrals.update)
 router.delete('/delete/:reffid',referrals.delete)
+=======
+const refferals=require('../controllers/referrals')
+
+router.get('/list',refferals.list)
+router.get('/parlist/:evid/:eval',refferals.parlist)
+// router.get('/list/:cuid/:eid',refferals.plist)
+router.post('/create',refferals.create)
+router.put('/enabordisa/:ieid',refferals.update)
+>>>>>>> Stashed changes
 
 module.exports = router
