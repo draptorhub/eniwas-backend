a967e4d04e608c0940b4192392f42ef8d77981a6
AnirudhKumaran <draptorteam@gmail.com>
2021-10-13 13:37:55 +0530

    refferal confusion

--- src/controllers/referrals.js
+++ src/controllers/referrals.js
@@ -4,59 +4,14 @@ const sequelize=require('../models/database')
 var table = sequelize.import('../models/referrals')
 sequelize.sync({force:false})
 
-<<<<<<< Updated upstream
-=======
-controllers.list = async (req, res) => {
-
-    const data = await table.findAll()
-    .then(function(data){
-        return data;
-    })
-    .catch(error => {
-        return error;
-    }); 
-
-    res.json({success : true, data : data});
-
-}
-
-controllers.parlist = async (req, res) => {
-
-  let ieid = req.params.evid
-  let ieval = req.params.eval
-
-  const data = await table.findAll({
-    where:{
-      eid:ieid,
-      is_enabled:ieval
-    },
-    attributes:['sid']
-  })
-  .then(function(data){
-      return data;
-  })
-  .catch(error => {
-      return error;
-  }); 
-
-  res.json({success : true, data : data});
-
-}
-
->>>>>>> Stashed changes
 controllers.create = async (req,res) => {
     
     console.log(req.body,new Date())
 
     const data = await table.create({
-<<<<<<< Updated upstream
       reffId:"",
       reffName:req.body.reffName,
       refLogo:req.body.refLogo
-=======
-      reffId:req.body.reffId,
-      reffName:req.body.reffName
->>>>>>> Stashed changes
     })
     .then(function(data){
       return data;
@@ -73,7 +28,6 @@ controllers.create = async (req,res) => {
     });
 }
 
-<<<<<<< Updated upstream
 controllers.list = async (req, res) => {
 
   const data = await table.findAll()
@@ -105,19 +59,6 @@ controllers.get = async (req, res) => {
   res.json({success : true, data : data});
 
 }
-=======
-controllers.delete = async (req, res) => {
-  // parameter post
-  const { id } = req.params.reffId;
-  // delete sequelize
-  const del = await Employee.destroy({
-    where: { reffId: id}
-  })
-  res.json({success:true,deleted:del,message:"Deleted successfully"});
-}
-
-// git remote set-url origin https://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@github.com/draptorhub/eniwas-backend.git
->>>>>>> Stashed changes
 
 controllers.update = async (req,res) => {
     
@@ -140,7 +81,6 @@ controllers.update = async (req,res) => {
     res.json({success:true, data:data, message:"Updated successful"});
 }
 
-<<<<<<< Updated upstream
 controllers.delete = async (req, res) => {
   
   // delete sequelize
@@ -150,6 +90,4 @@ controllers.delete = async (req, res) => {
   res.json({success:true,deleted:del,message:"Deleted successfully"});
 }
 
-=======
->>>>>>> Stashed changes
 module.exports=controllers
